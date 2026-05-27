7350ae0b5c71c675d162c7d992038d5b81f5fb66
Job Daniel Correa <jobcorre0912@gmail.com>
2026-05-26 21:47:12 -0500

    Puebas de objetos Descuentos

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main{
     public static void main(String[] args) {
         
+        Descuento porcentaje = new DescuentoPorcentaje(20);
+        Descuento fijo = new DescuentoFijo(5000);
+
+        porcentaje.mostrarDescuento(50000);
+        fijo.mostrarDescuento(15000);
     }
 }
